fba479dd6280d7764c1648f145f4fe84ffe2b4d1
ashu <207ashu@gmail.com>
2018-05-06 13:49:04 +0530

    WebView Implementation

---
+++ app/src/main/java/com/example/ashu/babypi/WebActivity3.java
@@ -0,0 +1,23 @@
+package com.example.ashu.babypi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebActivity3 extends AppCompatActivity {
+    WebView webView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web3);
+        webView=findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://10.1.201.162:5000#new");
+
+
+    }
+}
